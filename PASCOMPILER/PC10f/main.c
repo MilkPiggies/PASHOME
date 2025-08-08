482f61cfb87500f7bf98b22d0571c340fcfa2e92
SaLtY Bacon <gaconbaming@gmail.com>
2025-08-08 18:20:06 -0230

    Multi upload: Main-finished goto if switch
    test.cpr made for CVE on copyparty, unrelated
    
    finished goto if part of PASCOMPILER/PC10f. SOURCE output files also edited, disgard.

--- PASCOMPILER/PC10f/main.c
+++ PASCOMPILER/PC10f/main.c
@@ -28,7 +28,23 @@ void removeZeros(char *data) {
 
 
 
-
+	char BNN[] = { "!N" };
+	char BNG[] = { "N" };
+	char BNZ[] = { "!Z" };
+	char BZE[] = { "Z" };
+	char BNO[] = { "!O" };
+	char BOV[] = { "O" };
+	char BNC[] = { "!C" };
+	char BCA[] = { "C" };
+	char BBE[] = { ">=" };
+	char BSE[] = { "<=" };
+	char BCC[] = { "!>" };
+	char BCS[] = { ">" };
+	char BNE[] = { "!=" };
+	char BEQ[] = { "=" };
+	char BPL[] = { "!<" };
+	char BMI[] = { "<" };
+	
 
 
 
@@ -70,7 +86,10 @@ int main() {
     char c;
     
     int gtc = 0;
-	
+	char cond[2];
+	char fbr[3];
+
+
 
 	//while ((c = fgetc(source_file)) != EOF) {
 	//        if (c == '\n') {
@@ -160,24 +179,245 @@ int main() {
 			// goto if
 			if (strstr(line, "if")) {
 				printf("testgotoif ");
+				//printf("%s ", BNN);
 				w = 1;
 				
 gotoloop0:
 	
-				sscanf(line, "goto if %5s", &label);
+				sscanf(line, "goto if %2s %5s", &cond, &label);
 				if (strcmp(g[w], label) == 0) {
-					printf("goto if found at %d: %s\n",	o[w], g[w]);
-					// what kind of branch? 
-					// where branch to?
+					printf("goto if %s found at %d: %s\n", cond, o[w], g[w]);
 					
-					//
+					
+							// FUCK THESE REPETITIVE IF STATEMENTS
+							// JUST FUCKING LOOP IT . -faggotmold
+					
+					
+				// what kind of branch? 
+				
+					// not neg
+					if (strcmp(cond, BNN) == 0) {
+						printf("YAY GOOD BNN MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNN $# 0000\n");
+					}
+					
+					// neg
+					if (strcmp(cond, BNG) == 0) {
+						printf("YAY GOOD BNG MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNG $# 0000\n");
+					}
+					
+					// not zero
+					if (strcmp(cond, BNZ) == 0) {
+						printf("YAY GOOD BNZ MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNZ $# 0000\n");
+					}
+					
+					// zero
+					if (strcmp(cond, BZE) == 0) {
+						printf("YAY GOOD BZE MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BZE $# 0000\n");
+					}
+					
+					// not overflown 
+					if (strcmp(cond, BNO) == 0) {
+						printf("YAY GOOD BNO MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNO $# 0000\n");
+					}
+					
+					// overflown
+					if (strcmp(cond, BOV) == 0) {
+						printf("YAY GOOD BOV MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BOV $# 0000\n");
+					}
+					
+					// not carrying
+					if (strcmp(cond, BNC) == 0) {
+						printf("YAY GOOD BNC MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNC $# 0000\n");
+					}
+					
+					// carrying
+					if (strcmp(cond, BCA) == 0) {
+						printf("YAY GOOD BCA MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BCA $# 0000\n");
+					}
+					
+					// bigger or equal
+					if (strcmp(cond, BBE) == 0) {
+						printf("YAY GOOD BBE MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BBE $# 0000\n");
+					}
+					
+					// smaller or equal
+					if (strcmp(cond, BSE) == 0) {
+						printf("YAY GOOD BSE MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BSE $# 0000\n");
+					}
+					
+					// not bigger
+					if (strcmp(cond, BCC) == 0) {
+						printf("YAY GOOD BCC MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BCC $# 0000\n");
+					}
+					
+					// bigger
+					if (strcmp(cond, BCS) == 0) {
+						printf("YAY GOOD BCS MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BCS $# 0000\n");
+					}
+					
+					// not equal
+					if (strcmp(cond, BNE) == 0) {
+						printf("YAY GOOD BNE MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BNE $# 0000\n");
+					}
+					
+					// equal
+					if (strcmp(cond, BEQ) == 0) {
+						printf("YAY GOOD BEQ MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BEQ $# 0000\n");
+					}
+					
+					// not smaller
+					if (strcmp(cond, BPL) == 0) {
+						printf("YAY GOOD BPL MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BPL $# 0000\n");
+					}
+					
+					// smaller
+					if (strcmp(cond, BMI) == 0) {
+						printf("YAY GOOD BMI MATCH\n");
+						//fprintf LDX $# (label location)
+						fprintf(output_file, "LDX $# %04x\n", o[w]);					
+						//fprintf SBRM $# 0000
+						fprintf(output_file, "SBRM $# 0000\n");					
+						//fprintf SBRK $# 0000
+						fprintf(output_file, "SBRK $# 0000\n");					
+						//fprintf BR? $# 0000
+						fprintf(output_file, "BMI $# 0000\n");
+					}
+					
+					
+					
+					// where branch to?
+						// see above switch statement's					
 					
 					// print stuff (lines written)
-					fprintf(output_file, "testgotoif0\n");
-					fprintf(output_file, "testgotoif1\n");
+					//fprintf(output_file, "testgoto0\n");
+					//fprintf(output_file, "testgoto1\n");
 					
+					
+										
 					// add according lines written
 					gtc++;
+					gtc++;
+					gtc++; 
 					gtc++;
 					// more?
 					
@@ -188,6 +428,9 @@ gotoloop0:
 				// if not equal, go back && add 1 to w
 				w++;
 				goto gotoloop0;
+			
+			
+			
 			
 			
 			
@@ -204,22 +447,29 @@ gotoloop1:
 				if (strcmp(g[w], label) == 0) {
 					printf("goto found at %d: %s\n", o[w], g[w]);
 					// where branch to?
+						// defined by ldx below (first fprintf, %04x/o[w])
 					
-					//
 					
 					// print stuff (lines written)
-					fprintf(output_file, "testgoto0\n");
-					//fprintf LDX $# (label location)
+					//fprintf(output_file, "testgoto0\n");
+					//fprintf(output_file, "testgoto1\n");
 					
-					fprintf(output_file, "testgoto1\n");
+					//fprintf LDX $# (label location)
+					fprintf(output_file, "LDX $# %04x\n", o[w]);
+					
 					//fprintf SBRM $# 0000
+					fprintf(output_file, "SBRM $# 0000\n");
+					
 					//fprintf SBRK $# 0000
+					fprintf(output_file, "SBRK $# 0000\n");
 					
 					//fprintf BR $# 0000
-					
+					fprintf(output_file, "BR $# 0000\n");
 					
 					
 					// add according lines written
+					gtc++;
+					gtc++;
 					gtc++;
 					gtc++;
 					// more?
@@ -241,16 +491,21 @@ gotoexit:
 	
 	
 	
-	
-	
+		
 	
 			
 				
-		} // else if {
+		} else if(strstr(line, "penis")) {			
 			// Maybe test this with a simple word (penis)?
-		
+			printf("penis found on line: %d\n", a);
+			// spacing
+			printf("\n");
 
-
+		} // else if {
+        
+        
+        
+        
         
         a++;	// tracks output file location
         		// need to change this to hex
